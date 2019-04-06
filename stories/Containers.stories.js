2c6c2cc0da8e105cbc599ef9215213b258f26b60
Martín M <skydiver@users.noreply.github.com>
2019-04-06 17:48:52 -0300

    Containers stories

---
+++ stories/Containers.stories.js
@@ -0,0 +1,36 @@
+import { storiesOf } from '@storybook/vue';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { withInfo } from 'storybook-addon-vue-info';
+import centered from '@storybook/addon-centered/vue';
+
+import Box from '../src/Containers/Box.vue';
+
+const stories = storiesOf('Containers', module);
+
+stories.addDecorator(withKnobs);
+stories.addDecorator(withInfo);
+stories.addDecorator(centered);
+
+stories.add(
+  'Box',
+  () => ({
+    components: { 'vt-box': Box },
+    template: `
+      <vt-box :title="title">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>
+      </vt-box>
+    `,
+    props: {
+      title: {
+        default: text('Box title', 'Box Title')
+      }
+    }
+  }),
+  {
+    info: true
+  }
+);
