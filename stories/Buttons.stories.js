eff00a70e4c267f00638287ee57c2c40ec60e902
Martín M <skydiver@users.noreply.github.com>
2019-04-06 14:39:03 -0300

    outline buttons story

--- stories/Buttons.stories.js
+++ stories/Buttons.stories.js
@@ -4,10 +4,12 @@ import centered from '@storybook/addon-centered/vue';
 
 import ButtonSimple from '../src/Widgets/Buttons/ButtonSimple.vue';
 import ButtonPill from '../src/Widgets/Buttons/ButtonPill.vue';
+import ButtonOutline from '../src/Widgets/Buttons/ButtonOutline.vue';
+import Button3D from '../src/Widgets/Buttons/Button3D.vue';
 
 import tailwindColors from './tailwind-colors';
 
-const componentProps = () => ({
+const baseButtonProps = () => ({
   label: {
     default: text('Label', 'Sample Button')
   },
@@ -31,12 +33,32 @@ stories.add('Simple', () => ({
   components: { 'vt-button-simple': ButtonSimple },
   template:
     '<vt-button-simple :label="label" :background-color="backgroundColor" :hover-color="hoverColor" :text-color="textColor" />',
-  props: componentProps()
+  props: baseButtonProps()
 }));
 
 stories.add('Pill', () => ({
   components: { 'vt-button-pill': ButtonPill },
   template:
     '<vt-button-pill :label="label" :background-color="backgroundColor" :hover-color="hoverColor" :text-color="textColor" />',
-  props: componentProps()
+  props: baseButtonProps()
+}));
+
+stories.add('Outline', () => ({
+  components: { 'vt-button-outline': ButtonOutline },
+  template:
+    '<vt-button-outline :label="label" :hover-color="hoverColor" :text-hover-color="textHoverColor" :text-color="textColor" />',
+  props: {
+    label: {
+      default: text('Label', 'Sample Button')
+    },
+    hoverColor: {
+      default: select('Hover color', Object.keys(tailwindColors), 'red')
+    },
+    textHoverColor: {
+      default: select('Text hov. color', Object.keys(tailwindColors), 'white')
+    },
+    textColor: {
+      default: select('Text color', Object.keys(tailwindColors), 'red-dark')
+    }
+  }
 }));
