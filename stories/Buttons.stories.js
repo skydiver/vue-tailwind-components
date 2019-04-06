88eb679f266cb7b12ebc4ae252d1512a8955c41c
Martín M <skydiver@users.noreply.github.com>
2019-04-06 14:31:18 -0300

    using Tailwind colors for knobs

--- stories/Buttons.stories.js
+++ stories/Buttons.stories.js
@@ -1,22 +1,24 @@
 import { storiesOf } from '@storybook/vue';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import centered from '@storybook/addon-centered/vue';
 
 import ButtonSimple from '../src/Widgets/Buttons/ButtonSimple.vue';
 import ButtonPill from '../src/Widgets/Buttons/ButtonPill.vue';
+
+import tailwindColors from './tailwind-colors';
 
 const componentProps = () => ({
   label: {
     default: text('Label', 'Sample Button')
   },
   backgroundColor: {
-    default: text('Backg. color', 'red')
+    default: select('Backg. color', Object.keys(tailwindColors), 'red')
   },
   hoverColor: {
-    default: text('Hover color', 'red-dark')
+    default: select('Hover color', Object.keys(tailwindColors), 'red-dark')
   },
   textColor: {
-    default: text('Text color', 'white')
+    default: select('Text color', Object.keys(tailwindColors), 'white')
   }
 });
 
@@ -28,13 +30,13 @@ stories.addDecorator(centered);
 stories.add('Simple', () => ({
   components: { 'vt-button-simple': ButtonSimple },
   template:
-    '<vt-button-simple :label="label" :background-color="backgroundColor" :hover-color="hoverColor" text-color="white" />',
+    '<vt-button-simple :label="label" :background-color="backgroundColor" :hover-color="hoverColor" :text-color="textColor" />',
   props: componentProps()
 }));
 
 stories.add('Pill', () => ({
   components: { 'vt-button-pill': ButtonPill },
   template:
-    '<vt-button-pill :label="label" :background-color="backgroundColor" :hover-color="hoverColor" text-color="white" />',
+    '<vt-button-pill :label="label" :background-color="backgroundColor" :hover-color="hoverColor" :text-color="textColor" />',
   props: componentProps()
 }));
