ff854453d9a38dfc68189dcc208b07886527f9f9
Martín M <skydiver@users.noreply.github.com>
2019-04-06 14:21:53 -0300

    working on buttons stories

--- stories/Buttons.stories.js
+++ stories/Buttons.stories.js
@@ -1,10 +1,40 @@
 import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered/vue';
 
 import ButtonSimple from '../src/Widgets/Buttons/ButtonSimple.vue';
+import ButtonPill from '../src/Widgets/Buttons/ButtonPill.vue';
 
-storiesOf('Buttons', module).add('Simple', () => ({
+const componentProps = () => ({
+  label: {
+    default: text('Label', 'Sample Button')
+  },
+  backgroundColor: {
+    default: text('Backg. color', 'red')
+  },
+  hoverColor: {
+    default: text('Hover color', 'red-dark')
+  },
+  textColor: {
+    default: text('Text color', 'white')
+  }
+});
+
+const stories = storiesOf('Buttons', module);
+
+stories.addDecorator(withKnobs);
+stories.addDecorator(centered);
+
+stories.add('Simple', () => ({
   components: { 'vt-button-simple': ButtonSimple },
-  template: '<vt-button-simple labe="Simple Button" />'
+  template:
+    '<vt-button-simple :label="label" :background-color="backgroundColor" :hover-color="hoverColor" text-color="white" />',
+  props: componentProps()
+}));
+
+stories.add('Pill', () => ({
+  components: { 'vt-button-pill': ButtonPill },
+  template:
+    '<vt-button-pill :label="label" :background-color="backgroundColor" :hover-color="hoverColor" text-color="white" />',
+  props: componentProps()
 }));
