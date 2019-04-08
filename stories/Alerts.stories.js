f151547daa0756ff6fabd9f91f74ca105963233a
Martín M <skydiver@users.noreply.github.com>
2019-04-07 21:45:22 -0300

    refactor Alert with icon component

--- stories/Alerts.stories.js
+++ stories/Alerts.stories.js
@@ -4,6 +4,22 @@ import centered from '@storybook/addon-centered/vue';
 
 import WithIcon from '../src/Alerts/WithIcon.vue';
 
+const alertContent =
+  '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>';
+
+const alertProps = () => ({
+  title: {
+    default: text('Title', 'Alert Title')
+  },
+  type: {
+    default: select(
+      'Arrow type',
+      ['warning', 'info', 'success', 'error'],
+      'success'
+    )
+  }
+});
+
 const stories = storiesOf('Alerts', module);
 
 stories.addDecorator(withKnobs);
@@ -13,23 +29,8 @@ stories.add(
   'with Icon',
   () => ({
     components: { 'vt-alert-icon': WithIcon },
-    template: `
-      <vt-alert-icon :title="title" :type="type">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, a! A culpa, incidunt magnam, ipsam quas aperiam id vero possimus architecto et nobis nisi, aliquid necessitatibus soluta dolorum non nihil.</p>
-      </vt-alert-icon>
-    `,
-    props: {
-      title: {
-        default: text('Title', 'Alert Title')
-      },
-      type: {
-        default: select(
-          'Arrow type',
-          ['warning', 'info', 'success', 'error'],
-          'success'
-        )
-      }
-    }
+    template: `<vt-alert-icon :title="title" :type="type">${alertContent}</vt-alert-icon>`,
+    props: alertProps()
   }),
   {
     info: true
